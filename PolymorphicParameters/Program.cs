b80b7ba20a984891ea0be6c0a7cbe4b3a80ea703
Ekrem Iscan <eiscan@outlook.com.tr>
2023-04-05 00:37:25 +0300

    Polymorphic practice

---
+++ PolymorphicParameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PolymorphicParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new List<Car>
+            {
+                new Audi(200, "blue", "A6"),
+                new BMW(300, "black", "X6")
+            };
+
+            foreach (var car in cars)
+            {
+                car.Repair();
+            }
+
+            Car bmwZ3 = new BMW(200, "black", "Z3");
+            Car audiA3 = new Audi(100, "green", "A3");
+            bmwZ3.ShowDetails();
+            audiA3.ShowDetails();
+
+            BMW bmwM5 = new BMW(330, "white", "M5");
+            bmwM5.ShowDetails();
+
+            Car carB = (Car)bmwM5;
+            carB.ShowDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
