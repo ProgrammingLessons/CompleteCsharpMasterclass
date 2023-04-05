129c35b61f66807d5431b1c7ccb0e66863230134
Ekrem Iscan <eiscan@outlook.com.tr>
2023-04-05 16:48:54 +0300

    event handler practice

---
+++ EventDelegate/Subscriber2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    public class Subscriber2
+    {
+        public void Subscribe2(Publisher m)    
+        {
+            m.Tick += HeardIt;               
+        }
+        private void HeardIt(Publisher m, EventArgs e)   
+        {
+            System.Console.WriteLine("Subscribe2 listening");
+        }
+    }
+}
