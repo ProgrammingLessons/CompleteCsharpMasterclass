ecf0f6dbc476b5be1df54be019d85b74b50f7665
eiscan <eiscan@outlook.com.tr>
2023-04-10 23:41:38 +0300

    Add Comments

--- ExpandoObjectExam1/Program.cs
+++ ExpandoObjectExam1/Program.cs
@@ -9,20 +9,23 @@ namespace ExpandoObjectExam1
     {
         static void Main(string[] args)
         {
+            // object is defined
             dynamic d = new ExpandoObject();
+            // A dictionary definition is made to keep the properties.
             IDictionary<string, object> members = d as IDictionary<string, object>;
-
+            // Two properties named Name and Job are added.
             members.Add("Name", "Ekrem");
             members.Add("Job", "Developer");
 
             Console.WriteLine(d.Name);
             Console.WriteLine(d.Job);
             d.Event = null;
+            // Event is defined
             d.Event += new EventHandler((sender, e) =>
             {
                 Console.WriteLine("Event Invoke");
             });
-
+            // Event is triggered
             d.Event("", new EventArgs());
         }
     }
